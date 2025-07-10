407cc0695bddb9c5e98aaba3cf9eb304f0a207ab
iamawez <awezmanyar1672@gmail.com>
2025-07-10 23:33:09 +0530

    first commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use('/api', blogRoutes);
 app.use('/api', contactRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
